c681743838dff0db681e3a216b5e00abd0cade93
Felipe Borges <lipex360@gmail.com>
2021-10-24 19:29:05 -0300

    swagger started

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,12 +1,16 @@
 import express from 'express'
 import 'dotenv/config'
+import swagger from 'swagger-ui-express'
 
 import { routes } from '@shared/infra/http/routes'
+import swaggerFile from './routes/swagger.json'
 
 const app = express()
 
 app.use(express.json())
 
 app.use(routes)
+
+app.use('/api-docs', swagger.serve, swagger.setup(swaggerFile))
 
 export { app }
