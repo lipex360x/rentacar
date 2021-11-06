05bb5e4a44cd88ef5a800a463c9aae5e7d84d6e3
Felipe Borges <lipex360@gmail.com>
2021-11-06 18:30:47 -0300

    swagger updated

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,9 +1,6 @@
 import 'reflect-metadata'
 import 'dotenv/config'
 import express from 'express'
-import swagger from 'swagger-ui-express'
-
-import swaggerFile from './routes/swagger.json'
 
 import { routes } from '@shared/infra/http/routes'
 import '@shared/infra/typeorm'
@@ -14,7 +11,5 @@ const app = express()
 app.use(express.json())
 
 app.use(routes)
-
-app.use('/api-docs', swagger.serve, swagger.setup(swaggerFile))
 
 export { app }
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express'
+import swagger from 'swagger-ui-express'
 
 import { categoriesRoutes } from './categories.routes'
 import { specificationsRoutes } from './specifications.routes'
+
+import swaggerFile from './swagger.json'
 
 const routes = Router()
 
@@ -11,5 +14,7 @@ routes.use('/specifications', specificationsRoutes)
 routes.get('/hello', (request, response) => {
   response.json({ message: 'Hello World' })
 })
+
+routes.use('/api-docs', swagger.serve, swagger.setup(swaggerFile))
 
 export { routes }
