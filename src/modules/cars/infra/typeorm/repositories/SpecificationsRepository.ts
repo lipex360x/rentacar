68b8b2e00fbf9d76d381b10ae5ca695cc6c4dd98
Felipe Borges <lipex360@gmail.com>
2021-12-25 21:23:46 -0300

    specification updated

--- src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
@@ -1,6 +1,7 @@
-import { Specification } from '@modules/cars/infra/typeorm/entities/Specification'
 import { getRepository, Repository } from 'typeorm'
-import { ICreateProps, IFindByNameProps, ISpecificationsRepository } from '../interfaces/ISpecificationsRepository'
+
+import Specification from '@modules/cars/infra/typeorm/entities/Specification'
+import ISpecificationsRepository, { ICreateProps, IFindByNameProps } from '@modules/cars/repositories/interfaces/ISpecificationsRepository'
 
 class SpecificationsRepository implements ISpecificationsRepository {
   private repository: Repository<Specification>
@@ -9,10 +10,10 @@ class SpecificationsRepository implements ISpecificationsRepository {
     this.repository = getRepository(Specification)
   }
 
-  async create ({ name, description }: ICreateProps): Promise<void> {
+  async create ({ name, description }: ICreateProps): Promise<Specification> {
     const specification = this.repository.create({ name, description })
 
-    await this.repository.save(specification)
+    return this.repository.save(specification)
   }
 
   async findByName ({ name }: IFindByNameProps): Promise<Specification> {
@@ -24,4 +25,4 @@ class SpecificationsRepository implements ISpecificationsRepository {
   }
 }
 
-export { SpecificationsRepository }
+export default SpecificationsRepository
--- src/modules/cars/repositories/index.ts
+++ src/modules/cars/repositories/index.ts
@@ -1,15 +1,15 @@
 import { container } from 'tsyringe'
 import ICategoriesRepositoryProps from '@modules/cars/repositories/interfaces/ICategoriesRepository'
 import CategoriesRepository from '@modules/cars/infra/typeorm/repositories/CategoriesRepository'
-// import { ISpecificationsRepository } from '@modules/cars/repositories/interfaces/ISpecificationsRepository'
-// import { SpecificationsRepository } from '@modules/cars/repositories/implementations/SpecificationsRepository'
+import ISpecificationsRepository from '@modules/cars/repositories/interfaces/ISpecificationsRepository'
+import SpecificationsRepository from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository'
 
 container.registerSingleton<ICategoriesRepositoryProps>(
   'CategoriesRepository',
   CategoriesRepository
 )
 
-// container.registerSingleton<ISpecificationsRepository>(
-//   'SpecificationsRepository',
-//   SpecificationsRepository
-// )
+container.registerSingleton<ISpecificationsRepository>(
+  'SpecificationsRepository',
+  SpecificationsRepository
+)
