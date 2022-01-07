0767e7c04d3df5419f86ae244932efaf0b39421b
Felipe Borges <lipex360@gmail.com>
2022-01-07 10:29:50 -0300

    middleware rate limiter added

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata'
 import 'express-async-errors'
 import 'dotenv/config'
 import express from 'express'
+import cors from 'cors'
 import { errors } from 'celebrate'
 
 import routes from '@shared/infra/http/routes'
@@ -11,9 +12,12 @@ import typeorm from '@shared/infra/typeorm'
 import mongodb from '@shared/infra/mongoose'
 import '@shared/containers'
 import '@shared/providers'
+import rateLimiter from '@shared/middlewares/rateLimiter'
 
 const app = express()
 
+app.use(cors())
+app.use(rateLimiter)
 app.use(express.json())
 
 app.use(routes)
---
+++ src/shared/middlewares/rateLimiter/index.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from 'express'
+import { RateLimiterRedis } from 'rate-limiter-flexible'
+import Redis from 'ioredis'
+
+import AppError from '@shared/errors/AppError'
+import cacheConfig from '@shared/config/cache'
+
+const redisClient = new Redis(cacheConfig.config.redis)
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'ratelimit',
+  points: 5,
+  duration: 1
+})
+
+export default async function rateLimiterMiddleware (request:Request, response:Response, next:NextFunction): Promise<void> {
+  try {
+    await limiter.consume(request.ip)
+
+    return next()
+  } catch {
+    throw new AppError('Too many requests', 429)
+  }
+}
